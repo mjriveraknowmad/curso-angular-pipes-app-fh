d6fe907ebf5b8b8fe48a066fefbc7d0c81837c46
Marcos José Rivera Souto <mjrivera@knowmadmood.com>
2026-09-24 14:41:57 +0200

    feat: Ejemplos de pipes con números

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/basic-page/basic-page.component'),
   },
   {
+    path: 'numbers',
+    title: 'Numbers Pipe',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
     path: '**',
     redirectTo: 'basic',
   },
